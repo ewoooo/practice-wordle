02cc5ad994785b5d0640f83780e2a111286ba940
Wooseong Lee <leews7752@gmail.com>
2025-05-19 08:43:44 +0900

    Add keyboard logic

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -6,29 +6,39 @@ const ROWS = [
   ["Z", "X", "C", "V", "B", "N", "M"],
 ];
 
-// function statusByLetters(validatedGuesses) {
-//   // [{letter: A, status: correct}, ...]
-//   const statusObj = {};
-//   const allLetters = validatedGuesses.flat();
+function statusByLetters(validatedGuesses) {
+  // [{letter: A, status: correct}, ...]
+  const statusObj = {};
+  const allLetters = validatedGuesses.flat();
 
-//   allLetters.forEach(({ letter, status }) => {
-//     statusObj[letter] = status;
-//   });
+  allLetters.forEach(({ letter, status }) => {
+    // initiate the status on rank
+    const currentLetterStatus = statusObj[letter];
 
-//   const RANKS = {
-//     correct: 1,
-//     misplaced: 2,
-//     incorrect: 3,
-//   };
+    if (currentLetterStatus === undefined) {
+      statusObj[letter] = status;
+      return;
+    }
 
-//   const currentRank =
+    const RANKS = {
+      correct: 1,
+      misplaced: 2,
+      incorrect: 3,
+    };
 
-//   // previous {letter: A, status: correct}
-//   // next {letter: A, status: misplaced}
+    // previous {letter: A, status: correct}
+    const currentRank = RANKS[currentLetterStatus];
 
-//   return statusObj;
-//   // {A: correct}
-// }
+    // next {letter: A, status: misplaced}
+    const nextRank = RANKS[status];
+
+    if (currentRank > nextRank) {
+      statusObj[letter] = status;
+    }
+  });
+
+  return statusObj;
+}
 
 function Keyboard({ validatedGuesses }) {
   const vlidatedLetters = statusByLetters(validatedGuesses);
